53656499a5b47254ba51a679be49053b30fbe3b4
Ilan Khmelnitsky <ilankhmel@gmail.com>
2022-11-30 17:24:10 +0200

    media-player random and btns

--- src/cmps/media-player.vue
+++ src/cmps/media-player.vue
@@ -1,103 +1,79 @@
 <template>
     <YouTube 
         src="https://www.youtube.com/watch?v=tCXGJQYZ9JA" 
-        @ready="onReady"
+        @ready="getDuration"
+        @state-change="onSongLoaded"
         ref="youtube" />
 
         <div class="controls">
-            <button @click="changeSong(-1)">Previous song</button>
-            <button @click="togglePlay">{{ (isPlayed) ? 'Pause' : 'Play' }}</button>
-            <!-- <button @click="this.$refs.youtube.pauseVideo()">Pause</button> -->
-            <button @click="changeSong(1)">Next song</button>
-            <button @click="toggleMute">{{ (isMute) ? 'Unmute' : 'Mute' }}</button>
+            <button @click="randomSong"><random-svg/></button>
+            <button @click="changeSong(-1)"><prev-svg/></button>
+            <button @click="togglePlay"> <play-svg v-if="!isPlayed"/> <stop-svg v-else /></button>
+            <button @click="changeSong(1)"><change-svg/></button>
+            <button @click="toggleMute"><sound-svg v-if="!isMute"/> <muted-svg v-else/> </button>
+            
+            <input @input="setVolume" type="range" min="0" max="100">
             <label>
                 TimeStamp:
+                <input v-if="currTime" @input="setTimestamp"  type="range" :value="currTime" min="0" :max='duration'>
             </label>
-            <input v-if="isLoaded" @input="setTimestamp"  type="range" :value="currTime" min="0" :max='songDuration'>
-            <input v-else type="range" :value="currentTime">
-            <label for="">
-
-                Volume:
-            </label>
-            <input @input="setVolume" type="range" min="0" max="100">
-            <span v-if="isLoaded">Length: {{ songLength }}</span>
-            <button @click="cue">Cue Song</button>
-            <!-- <button @click="this.$refs.youtube.getVideoUrl()">Copy Link</button> -->
-            <button @click="currentTime">GetCurrTime</button>
-            <span>currTime: {{ currTime }}</span>
+            <span v-if="currTime">Length: {{ formattedTime(duration) }}</span>
+            <span>currTime: {{ formattedTime(currTime) }}</span>
         </div>
 </template>
 
 <script>
 import { defineComponent } from 'vue'
 import YouTube from 'vue3-youtube'
+import { utilService } from '../services/util.service'
+
+import playSvg from '../assets/svgs/media-player-play.vue'
+import stopSvg from '../assets/svgs/media-player-stop.vue'
+import changeSvg from '../assets/svgs/media-player-change.vue'
+import prevSvg from '../assets/svgs/media-player-prev.vue'
+import randomSvg from '../assets/svgs/media-player-random.vue'
+import soundSvg from '../assets/svgs/media-player-sound.vue'
+import mutedSvg from '../assets/svgs/media-player-muted.vue'
 
 export default defineComponent({
 
     data(){
         return{
-            isLoaded: false,
-            song: '',
             isPlayed: false,
             isMute: false,
             currSongIdx: 0,
             songList:['1w7OgIMMRc4', 'jNQXAC9IVRw','-tWhPv2U6aQ'],
-            currTime: '',
-            songDuration: '',
+            currTime: 0,
+            duration: null,
+            timeInterval: '',
         }
-    },
-
-    mounted(){
-        
-        // setTimeout(()=>{
-            this.loadPlaylist()
-            // this.currentTime()
-        // },20)
     },
     
     methods: {
+        
+        onSongLoaded(e){    
+            // clearInterval(this.timeInterval)      
+            if(e.data === 1){
+                this.getDuration()
+                updateCurrTime() 
+            }
 
-        loadPlaylist(){
-
-            //For playing youtube native playlist:
-            // try{
-
-                // setTimeout(()=>{
-                //     console.log('L');
-                //     this.$refs.youtube.loadPlaylist({
-                //     list:  'PLPCwOARniRc5tzHxrB-ZKkQVK3pTZ-9OU',
-                //     listType: 'playlist'
-                // })
-                // }, 2000)
-
-
-            // }catch(err){
-                // console.log(err);
-            // }
-        },
-
-        onReady() {
-            // this.$refs.youtube.playVideo()
-            this.loaded = true
-            // this.songLength
+            
         },
 
         togglePlay(){
             if(this.isPlayed){
                 this.$refs.youtube.pauseVideo() 
                 this.isPlayed = false
+                clearInterval(this.timeInterval)
             }else{
                 this.$refs.youtube.playVideo() 
                 this.isPlayed = true
-                this.currentTime()
+                this.updateCurrTime()
             }
-            this.isLoaded = true
-
-            console.log(this.isPlayed);
         },
         
         toggleMute(){
-            console.log('works');
             if (this.isMute){
                 this.$refs.youtube.unMute() 
                 this.isMute = false
@@ -113,77 +89,73 @@ export default defineComponent({
         },
 
         changeSong(dir){
-            if(dir > 0){
-                // this.$refs.youtube.nextVideo()
-                this.currSongIdx += 1
-                console.log(this.currSongIdx);
-                this.$refs.youtube.loadVideoById(this.songList[this.currSongIdx])
-            }else{
-                this.currSongIdx -= 1
-                this.$refs.youtube.loadVideoById(this.songList[this.currSongIdx])
-                // this.$refs.youtube.previousVideo()
-            }
+            this.currSongIdx += dir
+            this.$refs.youtube.loadVideoById(this.songList[this.currSongIdx])
+          
             this.isPlayed = true
+            
             // Video URL expample http://www.youtube.com/v/VIDEO_ID?version=3
-            // this.$refs.youtube.loadPlaylist(this.songList)
-            // this.$refs.youtube.playVideoAt(0)
+           
         },
 
         setTimestamp(e){
             const timeStamp = e.target.value
             this.$refs.youtube.seekTo(timeStamp)
-            console.log(timeStamp);
         },
 
-        cue(){
-            this.$refs.youtube.cueVideoById('gEPmA3USJdI')
-        },
-
-        currentTime(){
-            if(this.isPlayed){
-                setInterval(()=>{
-                    // console.log(Math.floor(this.$refs.youtube.getCurrentTime()).toString());
-                    this.currTime = Math.floor(this.$refs.youtube.getCurrentTime())
-                },1000)
-            }
-            // return Math.floor(this.$refs.youtube.getCurrentTime()).toString()
-        }
-    },
-
-    computed:{
-        songLength(){
-            const duration = Math.floor(this.$refs.youtube.getDuration())
-            console.log('NewSong',duration);
-
-            // if(duration < 60){
-            //     return '0:' + duration.toString()
+        updateCurrTime(){
+            // if(this.isPlayed){
+                this.timeInterval = setInterval(()=>{
+                        const currTime = this.$refs?.youtube?.getCurrentTime()
+                        console.log(currTime);
+                        if(currTime){
+                            this.currTime = Math.floor(currTime)
+                        } 
+                },1000)         
             // }
-            
-            const minutes = ('minutes:,', Math.floor(duration / 60))
-            var seconds = ('seconds:,', Math.floor(((duration / 60) % 1) * 60).toString());
-            console.log('min', minutes, 'sec', seconds);
-            // if(seconds.length < 2){
-            //     seconds = '0' + seconds
-            // }
-            // console.log('duration', minutes, ':', seconds);
-            // return minutes + ':' + seconds
-            return minutes.toString() + ':' + seconds
         },
 
         getDuration(){
+            // setTimeout(()=>{
+                console.log(this.$refs.youtube.getDuration());
+                console.log('Loaded');
+                this.duration = Math.floor(this.$refs.youtube.getDuration())
+            // },800)
+        },
+       
+        formattedTime(time){
+            const duration = time
+            const minutes = ('minutes:,', Math.floor(duration / 60))
+            var seconds = ('seconds:,', Math.floor(((duration / 60) % 1) * 60).toString());
+            // if(seconds.length < 2){
+            //    seconds = '0' + seconds 
+            // } 
+            return minutes.toString() + ':' + seconds
+        },
 
-            // setInterval(()=>{
-                const duration = Math.floor(this.$refs.youtube.getDuration())
-                console.log(duration / 60);
-                this.songDuration = duration
-                console.log(duration);
-                return duration.toString()
-            // },1000)
+        randomSong(){
+            const randomRange = this.songList.length - 1
+            const randIdx = utilService.getRandomIntInclusive(0, randomRange)
+            this.$refs.youtube.loadVideoById(this.songList[randIdx])
+        },
+    },
+
+    computed:{
+        playBtnStyle(){
+            // return (this.isPlayed) ? <play-svg /> : <stop-svg/>
         }
     },
-    
 
-    components: { YouTube },
+    components: {
+        YouTube,
+        playSvg,
+        stopSvg,
+        changeSvg,
+        prevSvg,
+        randomSvg,
+        soundSvg,
+        mutedSvg,
+        },
 })
 </script>
 
