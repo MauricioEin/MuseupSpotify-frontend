b521b86d92fed7ad501698226a19b0eff343a5bb
MauricioEin <mauricio.ein@gmail.com>
2022-12-01 21:04:35 +0200

    small volume bug

--- src/cmps/media-player.vue
+++ src/cmps/media-player.vue
@@ -78,7 +78,7 @@ export default defineComponent({
             isLoop: false,
             isShuffled: false,
             isFullscreen: false,
-            volume:0,
+            volume:50,
             currSongIdx: 0,
             originalList: ['nukZQTFsA10', 'ogCih4OavoY','-tWhPv2U6aQ'],
             songList: ['nukZQTFsA10', 'ogCih4OavoY','-tWhPv2U6aQ'],
