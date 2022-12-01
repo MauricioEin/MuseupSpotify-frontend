f4d87faf4290c60c7b3e4b99b15c5e70b3797375
MauricioEin <mauricio.ein@gmail.com>
2022-12-01 20:23:42 +0200

    minimenu fix

--- src/cmps/mini-opts-menu.vue
+++ src/cmps/mini-opts-menu.vue
@@ -1,7 +1,7 @@
 <template>
     <section class="mini-menu">
         <button v-for="action in actions" :key="action"
-         @click="$emit(`${action.split(' ')[0].toLowerCase()}`)">
+         @click="$emit(`${action.split(' ').join('').toLowerCase()}`)">
          {{ action }}
         </button>
     </section>
