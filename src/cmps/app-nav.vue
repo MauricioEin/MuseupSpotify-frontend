85be2245b0ad35b78d979da4c9c8228b6683f4a7
MauricioEin <mauricio.ein@gmail.com>
2022-12-03 14:47:46 +0200

    no more ministation just _id

--- src/cmps/app-nav.vue
+++ src/cmps/app-nav.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="nav-container">
     <div class="logo pointer" @click="$router.push('/station')">
-      <logo-svg /> <span>Sprintify</span>
+      <logo-svg /> <span>WannaPlay</span>
     </div>
     <nav class="permanent-nav">
       <ul class="app-nav clean-list">
