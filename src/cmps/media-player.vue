b7874a2317787b22215e7d11d3ea2a90b1e57aac
Ilan Khmelnitsky <ilankhmel@gmail.com>
2022-12-05 13:37:07 +0200

    shuffleFix2

--- src/cmps/media-player.vue
+++ src/cmps/media-player.vue
@@ -158,8 +158,13 @@ export default defineComponent({
         },
 
         changeSong(dir) {
-            var newIdx = (this.currSongIdx + dir + this.songList.length) % this.songList.length
-            if(this.isShuffled) newIdx = utilService.getRandomIntInclusive(0, this.songList.length - 1)
+            var newIdx = this.currSongIdx
+            if(this.isShuffled){
+                while(newIdx === this.currSongIdx){
+                    newIdx = utilService.getRandomIntInclusive(0, this.songList.length - 1)
+                }
+            }else   newIdx = (this.currSongIdx + dir + this.songList.length) % this.songList.length
+             
             // this gives us the calc for looping around the playlist
             this.$store.commit({ type: 'playStation', station: this.playingStation, idx: newIdx })
         },
