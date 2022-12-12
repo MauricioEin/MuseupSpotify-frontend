fcd4d3e179274276b1017839a5f83c4f20b58584
Tomer Shemer <tomershemer@gmail.com>
2022-12-12 12:23:51 +0200

    added login modal in library and liked songs

---
+++ src/cmps/login-modal.vue
@@ -0,0 +1,19 @@
+<template>
+    <section class="login-modal">
+        <h1>Please log in or sign up to start {{ action }}</h1>
+        <div class="flex align-center justify-center">
+            <button @click="$router.push('/login')">Login</button>
+            <button @click="$router.push('/signup')">Signup</button>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    props: {
+        action: {
+            type: String
+        }
+    }
+}
+</script>
